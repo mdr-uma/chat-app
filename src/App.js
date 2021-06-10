6be7023f48ae92007b9eafafbd27b2ff7ce85031
github username <um_2003_8@hotmail.com>
2021-06-10 11:07:51 -0400

    create reference of firebase auth and firestore to make it easy to use

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 const App = () => {
     return <h1>We Chat App</h1>
 }
